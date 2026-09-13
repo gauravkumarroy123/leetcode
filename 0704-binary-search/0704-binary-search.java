b6a5ce195d82fb5696b4c4d6e86b220ecfc608e5
gauravkumarroy123 <gauravsauravroy@gmail.com>
2026-09-14 02:05:53 +0530

    Time: 0 ms (100%), Space: 48.6 MB (13.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -10,9 +10,9 @@ class Solution {
          if(nums[mid]<target){
             left=mid+1;
         }
-        else{
-            right=mid-1;
-        }                                 
+        if(nums[mid]>target){
+        right=mid-1;                               
+        }
     } 
     return -1;   
     }
